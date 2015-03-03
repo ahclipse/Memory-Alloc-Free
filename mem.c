a559172fbe964e3ceb9dfa853880a1e8c0868300
Benjamin Richardson <brichard@galapagos-06.cs.wisc.edu>
2015-03-02 19:24:53 -0600

    Init started

--- mem.c
+++ mem.c
@@ -1,13 +1,3 @@
-/******************************************************************************
- * MODIFIED BY: Benjamin Richardson, Section 1 
- * Date: 12/8/14
- *
- * FILENAME: mem.c
- * AUTHOR:   cherin@cs.wisc.edu <Cherin Joseph>
- * DATE:     20 Nov 2013
- * PROVIDES: Contains a set of library functions for memory allocation
- ******************************************************************************/
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -17,7 +7,6 @@
 #include <string.h>
 #include "mem.h"
 
-/* this structure serves as the header for each block */
 typedef struct block_hd{
   /* The blocks are maintained as a linked list */
   /* The blocks are ordered in the increasing order of addresses */
@@ -41,70 +30,78 @@ typedef struct block_hd{
 
 }block_header;
 
-/* Global variable - This will always point to the first block */
-/* ie, the block with the lowest address */
 block_header* list_head = NULL;
+void* nextRegionStartAddress;
+void* slabHead = NULL;
 
 
-/* Function used to Initialize the memory allocator */
-/* Not intended to be called more than once by a program */
-/* Argument - sizeOfRegion: Specifies the size of the chunk which needs to be allocated */
-/* Returns 0 on success and -1 on failure */
-int Mem_Init(int sizeOfRegion)
+void* Mem_Init(int regionSize, int slabSize)
 {
-  int pagesize;
-  int padsize;
   int fd;
   int alloc_size;
-  void* space_ptr;
-  static int allocated_once = 0;
+  void* memStart;
+  int nextRegionOffset;
+  static int initialized = 0;
   
   if(0 != allocated_once)
   {
-    fprintf(stderr,"Error:mem.c: Mem_Init has allocated space during a previous call\n");
-    return -1;
+    return NULL;
   }
   if(sizeOfRegion <= 0)
   {
-    fprintf(stderr,"Error:mem.c: Requested block size is not positive\n");
-    return -1;
+    return NULL;
   }
 
-  /* Get the pagesize */
-  pagesize = getpagesize();
-
-  /* Calculate padsize as the padding required to round up sizeOfRegio to a multiple of pagesize */
-  padsize = sizeOfRegion % pagesize;
-  padsize = (pagesize - padsize) % pagesize;
-
-  alloc_size = sizeOfRegion + padsize;
-
-  /* Using mmap to allocate memory */
+  //mmap to allocate memory
   fd = open("/dev/zero", O_RDWR);
   if(-1 == fd)
   {
-    fprintf(stderr,"Error:mem.c: Cannot open /dev/zero\n");
-    return -1;
+    return NULL;
   }
-  space_ptr = mmap(NULL, alloc_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  memStart = mmap(NULL, regionSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
   if (MAP_FAILED == space_ptr)
   {
-    fprintf(stderr,"Error:mem.c: mmap cannot allocate space\n");
-    allocated_once = 0;
-    return -1;
+    return NULL;
   }
   
-  allocated_once = 1;
+  initialized = 1;
   
-  /* To begin with, there is only one big, free block */
-  list_head = (block_header*)space_ptr;
+  //TODO create slab
+  //TODO assuming instuctors are nice and slab size fits well in a quarter of the 
+  //	allocated space
+  nextRegionStartAddr = .25*(regionSize)+memStart;
+  
+  if(slabSize <= 0)
+  {
+    return NULL;
+  }
+
+  init_slab(slabSize ,nextRegionStartAddr);
+  if( slabHead == NULL )
+  {
+    return NULL;
+  }
+
+  list_head = (block_header*)(nextRegionAddr);
   list_head->next = NULL;
-  /* Remember that the 'size' stored in block size excludes the space for the header */
-  list_head->size_status = alloc_size - (int)sizeof(block_header);
   
-  return 0;
+  return memStart;
 }
 
+void init_slab( int slabSize, void* s_regionStart,  int s_regionSize)
+{
+  slabHead = s_regionStart;
+  
+  slabHead
+}
+
+void* Mem_Alloc(int size)
+{
+  if( size == slabSize)
+    return Mem_alloc_slab();
+  else
+    return Mem_Alloc_nextFit(size);
+}
 
 /* Function for allocating 'size' bytes. */
 /* Returns address of allocated block on success */
@@ -114,8 +111,7 @@ int Mem_Init(int sizeOfRegion)
 /* - Round up size to a multiple of 4 */
 /* - Traverse the list of blocks and allocate the best free block which can accommodate the requested size */
 /* -- Also, when allocating a block - split it into two blocks when possible */
-/* Tips: Be careful with pointer arithmetic */
-void* Mem_Alloc(int size)
+void* Mem_Alloc_nextFit(int size)
 {
   block_header *current;
   block_header *newblock;
@@ -218,7 +214,7 @@ void* Mem_Alloc(int size)
 /* - Return -1 if ptr is not pointing to the first byte of a busy block */
 /* - Mark the block as free */
 /* - Coalesce if one or both of the immediate neighbours are free */
-int Mem_Free(void *ptr)
+int Mem_Free_nextFit(void *ptr)
 {
   /* Pointers to block headers */
   block_header *previous;
