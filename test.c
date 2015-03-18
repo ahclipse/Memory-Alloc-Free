ce4d37bec7a0e9ed11e1a88d4bceb77558aeffb5
Benjamin Richardson <brichard@galapagos-20.cs.wisc.edu>
2015-03-17 19:33:38 -0500

    Updated tests

--- test.c
+++ test.c
@@ -13,6 +13,16 @@ int main(int argc, char * argv[])
   if((mem = Mem_Init(regionSize, slabSize)) != NULL)
   {
     Mem_Dump();
+    void* ptr = Mem_Alloc(256);
+    Mem_Dump();
+    void* ptr2 = Mem_Alloc(256);
+    Mem_Dump();
+    void* ptr3 = Mem_Alloc(256);
+    Mem_Dump();
+    Mem_Free(ptr);
+    Mem_Dump();
+    Mem_Free(ptr3);
+    Mem_Dump();
   }
 
   exit(0);
